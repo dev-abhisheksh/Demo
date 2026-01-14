16d257a204b71a9b7c0e12d045386c9695516524
niyazkhan-0 <niyazkhan987654321@gmail.com>
2026-01-14 22:54:03 +0530

    Add login page UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Hero from './component/Hero';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +11,7 @@ function App() {
   return (
     <>
       <div>
+        <Hero/>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
